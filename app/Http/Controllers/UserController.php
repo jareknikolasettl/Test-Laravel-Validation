f183184c3946bc924e935c5d76c25d08d9239f57
datasandwich <jarekettl98@gmail.com>
2023-09-10 20:06:46 +0100

    P7.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,9 @@ class UserController extends Controller
     {
         // TASK: change this line to not allow is_admin field to be updated
         // Update only the fields that are validated in UpdateUserRequest
-        $user->update($request->all()->except('is_admin'));
+        $user->update($request->except('is_admin'));
+
+
 
         return 'Success';
     }
